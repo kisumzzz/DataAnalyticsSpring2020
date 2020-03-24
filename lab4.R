189e954689b3e77791919ce5fb859446a51129a2
anqi zuo <46882827+kisumzzz@users.noreply.github.com>
2020-03-24 00:00:25 -0400

    Add files via upload

--- lab4.R
+++ lab4.R
@@ -227,3 +227,51 @@ classif<-knn(train,test,cg,k=5)
 classif
 attributes(.Last.value) 
 
+# Lab3_ctree
+require(rpart)
+swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
+
+require(party)
+library(party)
+
+treeSwiss<-ctree(Species ~ ., data=iris)
+plot(treeSwiss)
+
+cforest(Species ~ ., data=iris, controls=cforest_control(mtry=2, mincriterion=0))
+
+treeFert<-ctree(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+
+cforest(Fertility ~ Agriculture + Education + Catholic, data = swiss, controls=cforest_control(mtry=2, mincriterion=0))
+# look at help info, vary parameters.
+
+library(tree)
+tr <- tree(Species ~ ., data=iris)
+tr
+tr$frame
+plot(tr)
+text(tr)
+#find "prettier" ways to plot the tree
+
+#ctree2
+# Conditional Inference Tree for Mileage
+fit2M <- ctree(Mileage~Price + Country + Reliability + Type, data=na.omit(cu.summary))
+summary(fit2M)
+# plot tree
+plot(fit2M, uniform=TRUE, main="CI Tree Tree for Mileage ")
+text(fit2M, use.n=TRUE, all=TRUE, cex=.8)
+
+#ctree3
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosisâ")
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+#titanic
+data(Titanic)
+tit.rapet <- rpart(Survived ~ ., data = Titanic)
+tit.ctree <-  ctree(Survived ~ ., data = Titanic)
+
+library(randomForest)
+tit.rf <- randomForest(Survived ~ ., data = Titanic)
+
